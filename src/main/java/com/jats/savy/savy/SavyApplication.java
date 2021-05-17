296804c043b120e847dcd98591dc99db9270b9ab
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-05-17 14:49:49 +0900

    init

---
+++ src/main/java/com/jats/savy/savy/SavyApplication.java
@@ -0,0 +1,13 @@
+package com.jats.savy.savy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavyApplication.class, args);
+	}
+
+}
