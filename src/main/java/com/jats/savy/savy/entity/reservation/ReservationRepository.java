0cc43383dfd5faf3cf0f7a50aad95de3740cb57c
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-05-17 18:42:27 +0900

    [UPDATE] ReservationRepository add Pageable list

--- src/main/java/com/jats/savy/savy/entity/reservation/ReservationRepository.java
+++ src/main/java/com/jats/savy/savy/entity/reservation/ReservationRepository.java
@@ -1,6 +1,9 @@
 package com.jats.savy.savy.entity.reservation;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Page<Reservation> findAllBy(Pageable pageable);
 }
