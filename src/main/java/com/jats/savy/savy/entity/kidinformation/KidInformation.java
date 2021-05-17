65bc88bd30dbf08684ca661e45e1e7615efd9641
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-05-17 15:09:16 +0900

    [ADD] kidInformation

---
+++ src/main/java/com/jats/savy/savy/entity/kidinformation/KidInformation.java
@@ -0,0 +1,45 @@
+package com.jats.savy.savy.entity.kidinformation;
+
+import com.jats.savy.savy.entity.reservation.Reservation;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "kid_information_tbl")
+public class KidInformation {
+
+    @Id
+    private Long reservationId;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd`T`hh:mm:SS")
+    private LocalDateTime birthDate;
+
+    @Column(nullable = false)
+    private String kidName;
+
+    @Column(nullable = false)
+    private String vaccination;
+
+    private String fetusName;
+
+    private String request;
+
+    private String caution;
+
+    private String giveLater;
+
+}
---
+++ src/main/java/com/jats/savy/savy/entity/kidinformation/KidInformationRepository.java
@@ -0,0 +1,6 @@
+package com.jats.savy.savy.entity.kidinformation;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KidInformationRepository extends CrudRepository<KidInformation, Long> {
+}
