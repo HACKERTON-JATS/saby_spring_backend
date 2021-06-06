e7a072f0a3f5bc380dd6fbf33553d640fd11ac94
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-06 20:44:26 +0900

    [UPDATE] docker-compose

--- src/main/java/com/jats/savy/savy/security/auth/AuthDetailsService.java
+++ src/main/java/com/jats/savy/savy/security/auth/AuthDetailsService.java
@@ -6,9 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
-
 @RequiredArgsConstructor
 @Service
 public class AuthDetailsService implements UserDetailsService {
@@ -17,7 +14,6 @@ public class AuthDetailsService implements UserDetailsService {
 
     @Override
     public AuthDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Map
         return adminRepository.findById(username)
                 .map(AuthDetails::new)
                 .orElseThrow(UserNotFoundException::new);
